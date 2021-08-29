e4f8ee630e9920386458a005b3059c7af191fb3d
Pumpkin <a760329881@qq.com>
2021-08-29 19:17:29 +0800

    增加内容

---
+++ Python/Python_Study/线程池_进程池/1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author  : Pumpkin
+# @Time    : 2021/8/21 19:15
+# @Software: PyCharm
+# @File    : 1.py
+# @Mail    : a760329881@qq.com
+
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
--- Python/demo_1/demo_1.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author  : Pumpkin
-# @Time    : 2021/8/21 18:00
-# @Software: PyCharm
-# @File    : demo_1.py
-# @Mail    : a760329881@qq.com
