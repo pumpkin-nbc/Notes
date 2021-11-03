c75da837abffc5f8e9f96f4415927bca8238bc1f
Pumpkin <a760329881@qq.com>
2021-11-03 23:25:28 +0800

    删除多余文件

--- Java/Code/code_1/src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        // 输出HelloWorld
-        System.out.println("Hello World");
-
-    }
-}
--- Java/Code/code_1/src/demo_02.java
+++
@@ -1,7 +0,0 @@
-public class demo_02 {
-    public static void main(String[] args) {
-        String a="10";
-        int num;
-        System.out.println(a);
-    }
-}
